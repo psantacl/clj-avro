a071257d0c1b01fa09ffdfafa20f83368874332f
Kyle <kburton@relaynetwork.com>
2011-08-01 10:14:47 -0400

    update to swank-clojure 1.4.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.clojars.relaynetwork/clj-avro "1.0.4"
   :description "Avro Wrapper for Clojure"
-  :dev-dependencies [[swank-clojure "1.2.1"]]
+  :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]]
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.apache.hadoop/avro "1.3.3"]
                  [org.clojure/clojure-contrib "1.2.0"]])
