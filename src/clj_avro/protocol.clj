188a8768cea830f06ef0e239fcc0cfd8b91a2f07
Paul and Kyle <eng@relaynetwork.com>
2011-12-08 11:49:45 -0500

    added json-decode/encode

--- src/clj_avro/protocol.clj
+++ src/clj_avro/protocol.clj
@@ -1,17 +1,15 @@
 (ns clj-avro.protocol
   (:require
-   [clj-avro.core :as avro])
+   [clj-avro.core :as avro]
+   [clojure.contrib.json :as json])
   (:use
    [clj-etl-utils.lang-utils :only [raise resource-as-string]]))
 
 
-
 (def *protocol-registry* (atom {}))
-
 
 (defn register-protocol [proto-name protocol]
   (swap! *protocol-registry* assoc proto-name protocol))
-
 
 (defn lookup-protocol [proto-name]
   (get @*protocol-registry* proto-name))
@@ -34,32 +32,29 @@
       (if-let [p (lookup-protocol proto)]
         (.getType p (name schema)))))
 
-
 (defn avro-encode! [proto-and-schema thing]
   (avro/freeze (get-schema proto-and-schema) thing))
 
 (defn avro-decode! [proto-and-schema s]
   (avro/thaw (get-schema proto-and-schema) s))
 
+;; NB: these are for 'avro light', they create and consume just basic
+;; JSON, but do not create or validate actual avro encdoed JSON.  They
+;; can be used in cases where you want to use lightweight JSON object
+;; (leaving out optional fields in their entirety), but still wish to
+;; have validation performed on the structure and content of the data
+;; structures.
+(defn json-decode! [type body]
+  (let [obj (json/read-json body)]
+    (avro-encode! type obj)
+    obj))
 
-(comment
+(defn json-decode [type body]
+  (json/read-json body))
 
-(register-protocol-from-file
- :wall
- "/home/superg/projects/rn-wall/rn-wall-service/resources/public/api/wall-api-v1.json")
+(defn json-encode! [type thing]
+  (avro-encode! type thing)
+  (json/json-str thing))
 
-(get-schema :wall "relay.APIResponse")
-
-(avro-decode!
- :wall/relay.APIResponse
- (avro-encode! :wall/relay.APIResponse {:status 1 :foo 3}))
-
-(APIResponse/to-json {:status 1})
-(APIResponse/parse "{\"status\": 1}")
-
-(to-json (APIResponse. 1))
-
-(get-schema :wall/relay.APIResponse)
-
-
-)
+(defn json-encode! [type thing]
+  (json/json-str thing))
