37e1c56e43a457e84977be2609929e1de0a2cef8
Paul Santa Clara <psantaclara@relaynetwork.com>
2012-05-28 12:59:57 -0400

    depend on 1.3 version of clj-etl-utils

--- project.clj
+++ project.clj
@@ -8,4 +8,5 @@
                  ;; [org.apache.avro/avro "1.5.4"]
                  [org.apache.avro/avro "1.6.1"]
                  [org.clojure/data.json "0.1.3"]
-                 [org.clojars.kyleburton/clj-etl-utils        "1.0.49-SNAPSHOT"]])
+                 [org.clojars.kyleburton/clj-etl-utils "1.3.1-SNAPSHOT"]
+])
